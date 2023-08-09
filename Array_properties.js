c22f3e31fbf36c3f29b528402c0e8e7c363dce29
mig29pandas <kausthubh.manda@gmail.com>
2023-08-09 22:45:45 +0530

    Added a few methods for Arrays

--- Array_properties.js
+++ Array_properties.js
@@ -1,3 +1,10 @@
 let Array1 = [1, 2, 3, "Kausthubh", [4, 5]]
+let Array2 = [6, 7, 8, 90]
+let Array3 = Array1.concat(Array2)
 
-//Properties of Arrays are attributes/meathods of the 
+//Properties of Arrays are attributes/meathods of the
+
+console.log(Array1.length)
+console.log(Array1.at(3))          //This return the element stored at the given index
+console.log(Array3)
+console.log()
