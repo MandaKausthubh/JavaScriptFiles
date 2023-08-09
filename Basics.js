6d341b6631a9eef6b4cd1e9609b02486c01a45f4
mig29pandas <kausthubh.manda@gmail.com>
2023-08-09 18:45:34 +0530

    Added a basic JS file

---
+++ Basics.js
@@ -0,0 +1,15 @@
+//This is how you place Comments.
+
+//DATA TYPES IN JAVASCRIPT:
+//Integers, floats, strings, Arrays, Objects.
+let int = 2;
+let float = 4.3;
+let string = "Hello World";
+let Array1 = [1, 2, 3, 4, 5, 6];
+
+//Declaring Functions
+function Multiplyby5(a){
+    return 5*a;
+}
+
+console.log(typeof(int), typeof(float), typeof(string), typeof(Array1))
