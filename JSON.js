70f780e41e04a35748657e3290aa27b4e6ead532
mig29pandas <kausthubh.manda@gmail.com>
2023-08-10 21:52:52 +0530

    Added a JSON file

---
+++ JSON.js
@@ -0,0 +1,23 @@
+//JAVA SCRIPT OBJECT NOTATION...(JSON)
+
+// Data Transfer between Client and Server.
+// 
+[
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    }
+]
+  
