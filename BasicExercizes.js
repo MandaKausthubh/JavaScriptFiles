ff727058e8ff941fcf8c4a0ea554151b0b115db2
mig29pandas <kausthubh.manda@gmail.com>
2023-08-15 13:50:43 +0530

    Basic JS Problems

--- BasicExercizes.js
+++ BasicExercizes.js
@@ -11,10 +11,43 @@ function PrintMultiplicationTable(n){
     }
 }
 
-PrintMultiplicationTable(90)
 
-// function ReverseArray(arr) {
-//     console.log(arr.push(1))
-// }
+function ReverseArray(arr) {
+    if(arr.length === 0) ;
+    else{
+        //console.log(arr);
+        let temp = arr.shift();
+        //console.log(arr);
+        ReverseArray(arr);
+        arr.push(temp);
+    }
+}
 
-// ReverseArray([6])
+function Sort_Numbers(arr, l, r) {
+    if(arr.length === 0 || l === r) ;
+    else{
+        //console.log(arr);
+        //console.log(((l+r)/2));
+        let mid = Math.floor((l+r)/2);
+        console.log(mid);
+        Sort_Numbers(arr, l, mid);
+        Sort_Numbers(arr, mid+1, r);
+
+        var temp = []
+        let i = l; let j = mid+1;
+        while(i < mid && j < r) {
+            if(arr[i] < arr[j]){temp.push(arr[i++]);}
+            else{temp.push(arr[j++]);}
+        }
+        while(i < mid){temp.push(arr[i++]);}
+        while(j < mid){temp.push(arr[j++]);}
+
+        for(let x = l; x <= r; x++) {
+            arr[x] = temp[x-l];
+        }
+    }
+}
+
+let arr = [6,3,2]
+Sort_Numbers(arr, 0, arr.length-1)
+console.log(arr)
